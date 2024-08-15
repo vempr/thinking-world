5004dcf01600f2c03c9aae818b98511794cdde2f
vempr <alexnguyena6@gmail.com>
2024-08-15 21:14:28 +0200

    padding for scrollable content

--- app/components/wrappers/DefaultLayout.tsx
+++ app/components/wrappers/DefaultLayout.tsx
@@ -1,3 +1,7 @@
 export function DefaultLayout({ children }: { children: React.ReactNode }) {
-  return <main className="flex-grow dark:bg-neutral-800">{children}</main>;
+  return (
+    <main className="flex-grow py-8 sm:py-12 dark:bg-neutral-800">
+      {children}
+    </main>
+  );
 }
