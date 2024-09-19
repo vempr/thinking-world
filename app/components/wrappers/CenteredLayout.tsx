4bf6d14790347fb86bc4d161bc7a51383287117f
vempr <alexnguyena6@gmail.com>
2024-09-19 20:40:44 +0200

    padding change

--- app/components/wrappers/CenteredLayout.tsx
+++ app/components/wrappers/CenteredLayout.tsx
@@ -1,6 +1,6 @@
 export function CenteredLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex flex-grow flex-col items-center justify-center py-10 sm:py-12 dark:bg-neutral-800">
+    <main className="flex flex-grow flex-col items-center justify-center py-8 dark:bg-neutral-800">
       {children}
     </main>
   );
--- app/components/wrappers/DefaultLayout.tsx
+++ app/components/wrappers/DefaultLayout.tsx
@@ -1,7 +1,3 @@
 export function DefaultLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="flex-grow py-10 sm:py-12 dark:bg-neutral-800">
-      {children}
-    </main>
-  );
+  return <main className="flex-grow py-8 dark:bg-neutral-800">{children}</main>;
 }
