48b5f77b0c802df436d236e2b62ffbd01bcee1da
vempr <alexnguyena6@gmail.com>
2024-10-14 20:31:31 +0200

    ui

--- app/routes/schedule/DateSwitcher.tsx
+++ app/routes/schedule/DateSwitcher.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "@remix-run/react";
 import { getMonthByJSDateNumber } from "./utils/getMonth";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 export default function DateSwitcher() {
   const date = new Date();
@@ -16,7 +17,7 @@ export default function DateSwitcher() {
   return (
     <div className="flex flex-row mb-3 items-center justify-center md:justify-normal">
       <button
-        className="w-8 h-8 text-sm bg-sky-500 hover:bg-sky-600 rounded-full transition-all duration-50 text-white"
+        className="flex justify-center items-center w-8 h-8 text-sm bg-transparent outline outline-1 outline-neutral-800 dark:outline-white rounded-full hover:opacity-80 active:opacity-50 text-black dark:text-white"
         onClick={() => {
           const params = new URLSearchParams();
           if (month === 0) {
@@ -29,13 +30,13 @@ export default function DateSwitcher() {
           setSearchParams(params);
         }}
       >
-        {"<"}
+        <ChevronLeft size={18} />
       </button>
-      <h2 className="text-black dark:text-white text-lg w-52 text-center font-medium">
+      <h2 className="text-black dark:text-white text-lg w-52 text-center font-light">
         {getMonthByJSDateNumber(month)} {year}
       </h2>
       <button
-        className="w-8 h-8 text-sm bg-sky-500 hover:bg-sky-600 rounded-full transition-all duration-50 text-white"
+        className="flex justify-center items-center w-8 h-8 text-sm bg-transparent outline outline-1 outline-neutral-800 dark:outline-white rounded-full hover:opacity-80 active:opacity-50 text-black dark:text-white"
         onClick={() => {
           const params = new URLSearchParams();
           if (month === 11) {
@@ -48,7 +49,7 @@ export default function DateSwitcher() {
           setSearchParams(params);
         }}
       >
-        {">"}
+        <ChevronRight size={18} />
       </button>
     </div>
   );
