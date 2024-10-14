1198404d00b15d3cae0af6669e1e5beefe3f709b
vempr <alexnguyena6@gmail.com>
2024-10-14 11:42:31 +0200

    error boundary

--- app/routes/schedule/CalendarSidebar.tsx
+++ app/routes/schedule/CalendarSidebar.tsx
@@ -16,6 +16,7 @@ import {
 import { action } from "../schedule.workshift/route.tsx";
 import WorkShift from "./sidebar_components/WorkShift.tsx";
 import { Input } from "~/components/ui/input.tsx";
+import { TriangleAlert } from "lucide-react";
 
 export function getCoolColor() {
   const coolColors: string[] = [
@@ -75,6 +76,17 @@ export default function CalendarSidebar({ data, error }: CalendarSidebarProps) {
   useEffect(() => {
     if (fetcher.data?.success) setModalOpen(false);
   }, [fetcher.state]);
+
+  if (error)
+    return (<div className="bg-black bg-opacity-60 dark:bg-opacity-30 rounded-lg p-4 lg:w-72 text-center">
+      <h2 className="font-medium text-xl md:text-3xl text-white mb-4">
+        Work Shifts
+      </h2>
+      <div className="bg-red-500 rounded-lg flex flex-row pr-3 items-center justify-center text-right text-white">
+        <TriangleAlert size="110" className="mr-1 ml-5" />
+        <p className="text-sm">Unexpected Error: "{error.message}" Please try again later.</p>
+      </div>
+    </div>);
 
   return (
     <div className="bg-black bg-opacity-60 dark:bg-opacity-30 rounded-lg p-4 lg:w-72 text-center lg:text-left">
