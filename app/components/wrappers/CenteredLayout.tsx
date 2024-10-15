232e9329ed01efdba05b46b655abe507497dcbd0
vempr <alexnguyena6@gmail.com>
2024-10-15 12:31:20 +0200

    gradient

--- app/components/wrappers/CenteredLayout.tsx
+++ app/components/wrappers/CenteredLayout.tsx
@@ -1,6 +1,6 @@
 export function CenteredLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex flex-grow flex-col items-center justify-center py-8 dark:bg-neutral-800">
+    <main className="flex flex-grow flex-col items-center justify-center py-8 bg-gradient-to-r from-sky-900 to-black">
       {children}
     </main>
   );
