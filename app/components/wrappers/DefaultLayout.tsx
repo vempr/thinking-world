4df91eafc296d6b7cbed5fb2557951e0b4f5b8ec
vempr <alexnguyena6@gmail.com>
2024-10-15 10:18:19 +0200

    layout max 100rem

--- app/components/wrappers/DefaultLayout.tsx
+++ app/components/wrappers/DefaultLayout.tsx
@@ -1,5 +1,5 @@
 export function DefaultLayout({ children }: { children: React.ReactNode }) {
   return <main className="flex justify-center items-center flex-grow py-8 dark:bg-neutral-900">
-    <div className="flex-1 max-w-[90rem]">{children}</div>
+    <div className="flex-1 max-w-[100rem]">{children}</div>
   </main>;
 }
