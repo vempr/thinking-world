6a17e5288e4c2bb9ecca4f99df144d67bd362982
vempr <alexnguyena6@gmail.com>
2024-10-15 12:26:19 +0200

    link to

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -20,7 +20,7 @@ export default function Heading() {
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link
-            to="/schedule"
+            to="/schedule/work"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Your Schedule
